b500577baea833d524b2c927766f310620fffdfb
Sina Azimi <sinaazm15@gmail.com>
2024-07-25 14:54:45 +0200

    formatted

--- get_updates.py
+++ get_updates.py
@@ -7,9 +7,9 @@ import argparse
 from dateutil.relativedelta import relativedelta
 
 # default values
-CHANGES_TYPE = 'edit|new'
+CHANGES_TYPE = "edit|new"
 CHANGE_COUNT = 5
-LATEST = 'false'
+LATEST = "false"
 START_DATE = None
 END_DATE = None
 FILE_NAME = None
@@ -22,123 +22,123 @@ SKOS = "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>"
 WIKIBASE = "PREFIX wikibase: <http://wikiba.se/ontology#>"
 
 # Define namespaces
-PREFIXES = WD + "\n" + WDT + "\n" + SCHEMA + "\n" + SKOS + "\n" + WIKIBASE + "\n" 
+PREFIXES = WD + "\n" + WDT + "\n" + SCHEMA + "\n" + SKOS + "\n" + WIKIBASE + "\n"
 
 EDIT_DELETE_RDFS = []
 EDIT_INSERT_RDFS = []
 NEW_INSERT_RDFS = []
 
+
 def get_wikidata_updates(start_time, end_time):
     # Construct the API request URL
     api_url = "https://www.wikidata.org/w/api.php"
     params = {
-        'action': 'query',
-        'list': 'recentchanges',
-        'rcstart': end_time,
-        'rcend': start_time,
-        'rclimit': CHANGE_COUNT,
-        'rcprop': 'title|ids|sizes|flags|user|timestamp',
-        'format': 'json',
-        'rctype': CHANGES_TYPE # Limit the type of changes to edits and new entities
+        "action": "query",
+        "list": "recentchanges",
+        "rcstart": end_time,
+        "rcend": start_time,
+        "rclimit": CHANGE_COUNT,
+        "rcprop": "title|ids|sizes|flags|user|timestamp",
+        "format": "json",
+        "rctype": CHANGES_TYPE,  # Limit the type of changes to edits and new entities
     }
     # Make the request
     response = requests.get(api_url, params=params)
     data = response.json()
     # Check for errors in the response
-    if 'error' in data:
-        print("Error:", data['error']['info'])
+    if "error" in data:
+        print("Error:", data["error"]["info"])
         return
-    changes = data.get('query', {}).get('recentchanges', [])
+    changes = data.get("query", {}).get("recentchanges", [])
     return changes
+
 
 def compare_changes(api_url, change):
     global NEW_INSERT_RDFS
     new_rev = change["revid"]
     old_rev = change["old_revid"]
     diff = ""
-    if change['type'] == 'new':
+    if change["type"] == "new":
         # Fetch the JSON data for the new entity
-        new_insert_statement = new_entity_rdf.main(change['title'])
+        new_insert_statement = new_entity_rdf.main(change["title"])
         print(new_insert_statement)
         NEW_INSERT_RDFS.append(new_insert_statement)
         return
-    elif change['type'] != 'edit':
-        print("Unsupported change type:", change['type'])
+    elif change["type"] != "edit":
+        print("Unsupported change type:", change["type"])
         return
-    elif change['type'] == 'edit':        
+    elif change["type"] == "edit":
         params = {
-            'action': 'compare',
-            'fromrev': old_rev,
-            'torev': new_rev,
-            'format': 'json'
+            "action": "compare",
+            "fromrev": old_rev,
+            "torev": new_rev,
+            "format": "json",
         }
-        
+
         response = requests.get(api_url, params=params)
         comparison_data = response.json()
-        if 'compare' in comparison_data:
+        if "compare" in comparison_data:
             # Fetch The HTML diff of the changes using compare API
-            diff = comparison_data['compare']['*'] 
-            convert_to_rdf(diff, change['title'])
+            diff = comparison_data["compare"]["*"]
+            convert_to_rdf(diff, change["title"])
         else:
             print("Comparison data unavailable.")
     return diff
 
+
 def convert_to_rdf(diff_html, entity_id):
     # need a subject, predicate and object for each change
     subject = entity_id
-    soup = BeautifulSoup(diff_html, 'html.parser')
+    soup = BeautifulSoup(diff_html, "html.parser")
     # Construct DELETE and INSERT statements
     delete_statements = []
     insert_statements = []
     global PREFIXES, EDIT_DELETE_RDFS, EDIT_INSERT_RDFS
-    rows = soup.find_all('tr')
+    rows = soup.find_all("tr")
     current_predicate = None
     for row in rows:
         # Process property names
-        if row.find('td', class_='diff-lineno'):
-            value = row.find('a')
+        if row.find("td", class_="diff-lineno"):
+            value = row.find("a")
             if value:
                 current_predicate = f"wdt:{value.text.strip()}"
                 # if ('wdt' not in PREFIXES): PREFIXES += WDT + "\n"
             else:
                 # if ('schema' not in PREFIXES): PREFIXES += SCHEMA + "\n"
-                current_predicate = f"schema:{row.find('td', class_='diff-lineno').text.strip()}"
-                    
+                current_predicate = (
+                    f"schema:{row.find('td', class_='diff-lineno').text.strip()}"
+                )
+
         # Process deleted values
-        if row.find('td', class_='diff-deletedline'):
-            value = row.find('del', class_='diffchange')
+        if row.find("td", class_="diff-deletedline"):
+            value = row.find("del", class_="diffchange")
             if value and current_predicate:
                 deleted_value = value.text.strip()
-                delete_statements.append(f"  wd:{subject} {current_predicate} \"{deleted_value}\" .")
+                delete_statements.append(
+                    f'  wd:{subject} {current_predicate} "{deleted_value}" .'
+                )
 
-        
         # Process added values
-        elif row.find('td', class_='diff-addedline'):
-            value = row.find('ins', class_='diffchange')
+        elif row.find("td", class_="diff-addedline"):
+            value = row.find("ins", class_="diffchange")
             if value and current_predicate:
                 added_value = value.text.strip()
-                insert_statements.append(f"  wd:{subject} {current_predicate} \"{added_value}\" .")
+                insert_statements.append(
+                    f'  wd:{subject} {current_predicate} "{added_value}" .'
+                )
 
+    delete_rdf = "DELETE DATA {\n" + "\n".join(delete_statements) + "\n};"
 
-    delete_rdf = (
-        "DELETE DATA {\n"
-        + "\n".join(delete_statements) +
-        "\n};"
-    )
-
-    insert_rdf = (
-        "INSERT DATA {\n"
-        + "\n".join(insert_statements) +
-        "\n};"
-    )
-    if(delete_statements != []):
+    insert_rdf = "INSERT DATA {\n" + "\n".join(insert_statements) + "\n};"
+    if delete_statements != []:
         EDIT_DELETE_RDFS.append(delete_rdf)
         print(delete_rdf)
         print("\n")
-    if (insert_statements != []):
+    if insert_statements != []:
         EDIT_INSERT_RDFS.append(insert_rdf)
         print(insert_rdf)
         print("\n")
+
 
 def verify_args(args):
     global CHANGES_TYPE, CHANGE_COUNT, LATEST, START_DATE, END_DATE, FILE_NAME
@@ -152,20 +152,24 @@ def verify_args(args):
         print("Cannot set end date without start date.")
         return False
     if args.type:
-        if  args.type not in ['edit|new','edit', 'new']:
-            print("Invalid type argument. Please provide 'edit' or 'new, not setting means both of them'.")
+        if args.type not in ["edit|new", "edit", "new"]:
+            print(
+                "Invalid type argument. Please provide 'edit' or 'new, not setting means both of them'."
+            )
             return False
         else:
             CHANGES_TYPE = args.type
     if args.latest:
-        if args.latest not in ['true', 'false']:
+        if args.latest not in ["true", "false"]:
             print("Invalid type argument. Please provide 'true' or 'false'.")
             return False
         else:
             LATEST = args.latest
     if args.file:
-        if not args.file.endswith('.ttl') and not args.file.endswith('.txt'):
-            print("Invalid file name. Please provide a file with .ttl or .txt extension.")
+        if not args.file.endswith(".ttl") and not args.file.endswith(".txt"):
+            print(
+                "Invalid file name. Please provide a file with .ttl or .txt extension."
+            )
             return False
         FILE_NAME = args.file
     if args.number:
@@ -179,27 +183,38 @@ def verify_args(args):
             print("Invalid start date argument. Please provide a valid date.")
             return False
         else:
-            START_DATE = datetime.strptime(args.start, '%Y-%m-%d %H:%M:%S')
+            START_DATE = datetime.strptime(args.start, "%Y-%m-%d %H:%M:%S")
     if args.end:
         if not verify_date(args.end):
             print("Invalid end date argument. Please provide a valid date.")
             return False
         else:
-            END_DATE = datetime.strptime(args.end, '%Y-%m-%d %H:%M:%S')
+            END_DATE = datetime.strptime(args.end, "%Y-%m-%d %H:%M:%S")
     if not args.start or not args.end:
-        LATEST = 'true'
+        LATEST = "true"
 
     return True
 
+
 def verify_date(date):
-    if type(date) is not str or len(date) != 19 or date[10] != ' ' \
-        or date[13] != ':' or date[16] != ':' or date[4] != '-' or date[7] != '-' \
-        or int(date[0:4]) not in range(1000, 9999) or int(date[5:7]) not in range(1, 12) \
-        or int(date[8:10]) not in range(1, 31) or int(date[11:13]) not in range(0, 24) \
-        or int(date[14:16]) not in range(0, 60) or int(date[17:19]) not in range(0, 60):
+    if (
+        type(date) is not str
+        or len(date) != 19
+        or date[10] != " "
+        or date[13] != ":"
+        or date[16] != ":"
+        or date[4] != "-"
+        or date[7] != "-"
+        or int(date[0:4]) not in range(1000, 9999)
+        or int(date[5:7]) not in range(1, 12)
+        or int(date[8:10]) not in range(1, 31)
+        or int(date[11:13]) not in range(0, 24)
+        or int(date[14:16]) not in range(0, 60)
+        or int(date[17:19]) not in range(0, 60)
+    ):
         return False
     # check if date is not earlier than 1 month ago from now
-    formatted_date = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+    formatted_date = datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
     now = datetime.now()
     one_month_ago = now - relativedelta(months=1)
     if formatted_date < one_month_ago:
@@ -207,29 +222,45 @@ def verify_date(date):
         return False
     return True
 
+
 def write_to_file(data):
     print("Writing changes to file...")
-    with open(FILE_NAME, 'w') as file:
+    with open(FILE_NAME, "w") as file:
         file.write(PREFIXES)
-        file.write('\n')
+        file.write("\n")
         for change_list in data:
             for change in change_list:
                 file.write(change)
-                file.write('\n')
+                file.write("\n")
     print("Changes written to file.")
 
-def main ():
+
+def main():
     # define some command line arguments
     parser = argparse.ArgumentParser(
-    description="This script retrieves recent changes of the wikidata, allowing you to store the output in a file"
-                "not setting a time period will get the latest changes"
+        description="This script retrieves recent changes of the wikidata, allowing you to store the output in a file"
+        "not setting a time period will get the latest changes"
     )
-    parser.add_argument("-f", "--file", help = "store the output in a file")
-    parser.add_argument("-l", "--latest", help = "get latest changes")
-    parser.add_argument("-t", "--type", help = "filter the type of changes. possible values are edit|new, edit, new")
-    parser.add_argument("-n", "--number", help = "number of changes to get, not setting will get 5 changes, Maximum number of changes is 500")
-    parser.add_argument("-st", "--start", help = "start date and time, in form of 'YYYY-MM-DD HH:MM:SS, not setting start and end date will get latest changes")
-    parser.add_argument("-et", "--end", help = "end date and time, in form of 'YYYY-MM-DD HH:MM:SS'")
+    parser.add_argument("-f", "--file", help="store the output in a file")
+    parser.add_argument("-l", "--latest", help="get latest changes")
+    parser.add_argument(
+        "-t",
+        "--type",
+        help="filter the type of changes. possible values are edit|new, edit, new",
+    )
+    parser.add_argument(
+        "-n",
+        "--number",
+        help="number of changes to get, not setting will get 5 changes, Maximum number of changes is 500",
+    )
+    parser.add_argument(
+        "-st",
+        "--start",
+        help="start date and time, in form of 'YYYY-MM-DD HH:MM:SS, not setting start and end date will get latest changes",
+    )
+    parser.add_argument(
+        "-et", "--end", help="end date and time, in form of 'YYYY-MM-DD HH:MM:SS'"
+    )
     args = parser.parse_args()
 
     # verify the arguments type and values
@@ -241,7 +272,7 @@ def main ():
         print("Start Date: ", START_DATE)
         print("End Date: ", END_DATE)
         print("File Name: ", FILE_NAME)
-        print('\n')
+        print("\n")
         print(PREFIXES)
         changes = get_wikidata_updates(START_DATE, END_DATE)
         # Calling compare changes with the first change in the list for demonstration
@@ -250,5 +281,6 @@ def main ():
         # write the changes to a file
         if FILE_NAME:
             write_to_file([EDIT_DELETE_RDFS, EDIT_INSERT_RDFS, NEW_INSERT_RDFS])
+
 
 main()
